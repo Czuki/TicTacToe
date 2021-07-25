c74fedc3dc7da7e30cf8e291a372289a7a47676d
Czuki <bart3322@gmail.com>
2021-07-25 19:12:05 +0200

    nazwa zmiennej

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
-def show_board(plansza):  # draws a board which is held in a list of lists
+def show_board(board):  # draws a board which is held in a list of lists
     for i in range(3):
         for j in range(3):
-            print(plansza[i][j],
+            print(board[i][j],
                   end='')  # empty 'end' argument in print statement to avoid printing new line when drawing rows
         print('\n')  # new line after full row is drawn
 
